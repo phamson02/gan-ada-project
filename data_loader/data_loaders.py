9d52f2e1cdf110c4f6e2594103348545fc6383a7
phamson02 <phamtienson02@gmail.com>
2022-11-25 10:29:16 +0700

    update default parameters

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -6,8 +6,8 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, pin_memory=True,
-                 drop_last=True, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, pin_memory=False,
+                 drop_last=False, training=True):
         trsfm = transforms.Compose([
             transforms.Resize(28),
             transforms.ToTensor(),
