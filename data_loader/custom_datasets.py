f298a2d4b15e2eaa443ae5761c605850c04cdd4b
phamquang-hieu <phamhieu1906@gmail.com>
2022-11-29 22:58:02 +0700

    custom dataset for cifar10 + celebA + ffhq added

---
+++ data_loader/custom_datasets.py
@@ -0,0 +1,25 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+import glob as glob
+import os
+from PIL import Image
+
+class CelebA64(Dataset):
+    def __init__(self, data_dir, transforms, train_portion=0.9):
+        self.data_dir = data_dir
+        self.transforms = transforms
+        self.img_paths = glob.glob(os.path.join(self.data_dir, "*.png"))
+        self.len = np.floor(len(self.img_paths)*train_portion).astype(int)
+        self.img_paths = self.img_paths[:self.len]
+
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, index):
+        # print("hello", len(self.img_paths), index)
+        img = Image.open(self.img_paths[index])
+
+        if self.transforms:
+            img = self.transforms(img)
+        return (img, 1)
--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,6 +1,6 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
-
+from data_loader.custom_datasets import *
 
 class MnistDataLoader(BaseDataLoader):
     """
@@ -15,4 +15,28 @@ class MnistDataLoader(BaseDataLoader):
         ])
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, pin_memory=pin_memory, drop_last=drop_last)
+
+class CelebA64DataLoader(BaseDataLoader):
+    def __init__(self, data_dir, batch_size, img_size=64, train_portion=0.9, shuffle=True, validation_split=0.0, num_workers=1, pin_memory=False, drop_last=False, training=True):
+        trsfm = transforms.Compose([
+                        transforms.Resize(img_size),
+                        transforms.CenterCrop(img_size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                        ])
+        self.data_dir = data_dir
+        self.dataset = CelebA64(self.data_dir, transforms=trsfm, train_portion=train_portion)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, pin_memory=pin_memory, drop_last=drop_last)
+
+class Cifar10DataLoader(BaseDataLoader):
+    def __init__(self, data_dir, batch_size, img_size=32, shuffle=True, validation_split=0.0, num_workers=1, pin_memory=False, drop_last=False, training=True):
+        self.data_dir = data_dir
+        trsfm = transforms.Compose([
+                        transforms.Resize(img_size),
+                        transforms.CenterCrop(img_size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                        ])
+        self.dataset = datasets.CIFAR10(self.data_dir, transform=trsfm, download=True)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, pin_memory=pin_memory, drop_last=drop_last)
