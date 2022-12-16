c27498820f80f64fb15980447470ab9e2957c146
phamquang-hieu <phamhieu1906@gmail.com>
2022-12-16 10:30:54 +0700

    bugfix fid

--- eval.py
+++ eval.py
@@ -31,19 +31,21 @@ import csv
 #         vutils.save_image(image.add(1).mul(0.5), f'{folder_name}/{i}.jpg')
 
 
-def main(config: ConfigParser):
+def main(config: ConfigParser, args):
     logger = config.get_logger('test')
 
     # build model architecture
     model = config.init_obj('arch', module_arch)
-    model_name = config.resume.split("/")[-1]
+    resume = args.resume
+    model_name = resume.split("/")[-1]
     logger.info(model)
     
-    ckpts = glob.glob(os.path.join(config.resume, "*.pth"))
+    ckpts = glob.glob(os.path.join(resume, "*.pth"))
+    # print(glob.glob("checkpoints/*.pth"))
     ckpts.sort()
-
+    os.makedirs(config['eval']['save_dir'], exist_ok=True)
     for i, ckpt in enumerate(ckpts):
-        logger.info('Loading checkpoint: {} ...'.format(config.resume))
+        logger.info('Loading checkpoint: {} ...'.format(resume))
         checkpoint = torch.load(ckpt)
         state_dict = checkpoint['state_dict']
         if config['n_gpu'] > 1:
@@ -67,35 +69,39 @@ def main(config: ConfigParser):
                     vutils.save_image(g_img.add(1).mul(0.5), 
                         os.path.join(config['eval']['save_dir'], '%d.png'%(i*config['eval']['batch_size']+j)))#, normalize=True, range=(-1,1))
         
-        fid_value = calculate_fid_given_paths((config['eval']['save_dir'], config.cs), batch_size=config['eval']['batch_size'], device=device, dims=2048, num_workers=1)
-        
-        with open(f"{model_name}.csv") as f:
+        fid_value = calculate_fid_given_paths((config['eval']['save_dir'], args.calculated_stats), batch_size=config['eval']['batch_size'], device=device, dims=2048, num_workers=1)
+        print(fid_value)
+        with open(f"{model_name}.csv", "a") as f:
             writer = csv.writer(f)
             writer.writerow([fid_value, ckpt.split("/")[-1].split(".")[0]])  
 
-        if config.clear_dir and i != len(ckpts)-1:
+        if args.clear_dir and i != len(ckpts)-1:
             os.remove(ckpt)
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='Generating images from trained GAN model')
-    args.add_argument('-c', '--config', default=None, type=str,
+    parser = argparse.ArgumentParser(description='Generating images from trained GAN model')
+    parser.add_argument('-c', '--config', default=None, type=str,
                       help='config file path (default: None)')
-    args.add_argument('-r', '--resume', default=None, type=str,
-                      help='path to checkpoint dir (default: None)')
-    args.add_argument('-d', '--device', default=None, type=str,
-                      help='indices of GPUs to enable (default: all)')
-    args.add_argument('-cs', '--calulated_stats', defaut="None", type=str,
-                        help="path to precalculated stats")
-    args.add_argument('-clr', "--clear_dir", default=False, action="store_true",
-                        help="whether or not cleaning the whole model's checkpoints dir except for the lastest checkpoint after calculation")        
 
+    parser.add_argument('-r', '--resume', default=None, type=str,
+                      help='path to checkpoint dir (default: None)')
+    parser.add_argument('-d', '--device', default=None, type=str,
+                      help='indices of GPUs to enable (default: all)')
+    parser.add_argument('-cs', '--calculated_stats', default="None", type=str,
+                        help="path to precalculated stats")
+    parser.add_argument('-clr', "--clear_dir", default=False, action="store_true",
+                        help="whether or not cleaning the whole model's checkpoints dir except for the lastest checkpoint after calculation")        
+    args = parser.parse_args()
     # custom cli options to modify configuration from default values given in json file.
     CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
+    
     options = [
         CustomArgs(['-dir', '--dist'], type=str, target='eval;save_dir'),
         CustomArgs(['-n', '--n_sample'], type=int, target='eval;n_sample'),
-        CustomArgs(['-bs', '--batch_size'], type=int, target='eval;batch_size')
+        CustomArgs(['-bs', '--batch_size'], type=int, target='eval;batch_size'),
+        CustomArgs(['-cs', '--calculated_stats'], type=str, target='eval;calculated_stats'),
     ]
-    config = ConfigParser.from_args(args, options)
-    
-    main(config)
+    config = ConfigParser.from_args(parser)
+    # config = ConfigParser.from_args(args)
+    # print(type(config.resume), config.resume)
+    main(config, args)
--- parse_config.py
+++ parse_config.py
@@ -61,7 +61,7 @@ class ConfigParser:
             os.environ["CUDA_VISIBLE_DEVICES"] = args.device
         if args.resume is not None:
             resume = Path(args.resume)
-            cfg_fname = resume.parent / 'config.json'
+            cfg_fname = Path(args.config)
         else:
             msg_no_cfg = "Configuration file need to be specified. Add '-c config.json', for example."
             assert args.config is not None, msg_no_cfg
