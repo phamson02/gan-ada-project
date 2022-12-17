0880c0663de509616a79315021b78fe28b6acfd7
phamquang-hieu <phamhieu1906@gmail.com>
2022-12-17 11:00:40 +0700

    remove expr id

--- parse_config.py
+++ parse_config.py
@@ -32,7 +32,7 @@ class ConfigParser:
         self._log_dir = save_dir / 'log' / exper_name
 
         # make directory for saving checkpoints and log.
-        exist_ok = run_id == ''
+        exist_ok = True
         self.save_dir.mkdir(parents=True, exist_ok=exist_ok)
         self.log_dir.mkdir(parents=True, exist_ok=exist_ok)
 
