b9b8d0ca84969bc3b418d2791460cb040669b94d
dat <dat27072002@gmail.com>
2022-12-23 09:44:56 +0700

    set buffer for lambda_t

--- augment/ada.py
+++ augment/ada.py
@@ -16,19 +16,20 @@ class Ada(BAug.AugmentPipe):
         self.name = "ADA"
         self.ada_target = ada_target
         self.integration_steps = integration_steps
+        assert integration_steps > 0, "Integration step must be a positive integer!"
         self.ada_kimg = ada_kimg
-        self.lambda_t = list()
+        self.register_buffer('lambda_t', torch.zeros([1]))
 
     def update_p(self, batch_size_D):
         # the augmentation probability is updated based on the dicriminator's
         # accuracy on real images
-        accuracy_error = (sum(self.lambda_t)/self.integration_steps).cpu() - self.ada_target
+        accuracy_error = self.lambda_t[1:].mean().cpu() - self.ada_target
         self.p.copy_(torch.as_tensor(torch.clamp(self.p + np.sign(accuracy_error) * \
                                                  batch_size_D * self.integration_steps / \
                                                  (1000 * self.ada_kimg), 0., 1.)))
 
     def update_lambda(self, lambda_t):
-        self.lambda_t.append(lambda_t)
+        self.lambda_t.data = torch.cat((self.lambda_t.data, lambda_t), 0)
 
     def reset_lambda(self):
-        self.lambda_t = list()
+        self.lambda_t.data = torch.zeros((1,))
